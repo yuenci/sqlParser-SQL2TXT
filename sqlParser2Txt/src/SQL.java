6ddadce62075694715c3480282f5a2904631e288
Innis <1575270674@qq.com>
2022-09-25 14:10:06 +0800

    Add Entrance，checker，sql operate files

---
+++ sqlParser2Txt/src/SQL.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class SQL {
+    public static boolean execute(String sql){
+        return  false;
+    }
+
+    public static ArrayList<String[]> query(String sql){
+        return new ArrayList<String[]>();
+    }
+}
---
+++ sqlParser2Txt/src/sqlAdd.java
@@ -0,0 +1,2 @@
+public class sqlAdd {
+}
