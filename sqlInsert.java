f008b33dc652cee8a5f4c075ca6c05e1d84c96b0
Innis <1575270674@qq.com>
2022-09-30 10:53:09 +0800

    add positioner

--- sqlInsert.java
+++ sqlInsert.java
@@ -1,4 +1,10 @@
 public class sqlInsert extends Operate {
+    private String SQLType;
+
+    public sqlInsert(String type) {
+        this.SQLType = type;
+    }
+
     public boolean excute(String sql) {
         return true;
     }
